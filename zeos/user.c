cb1fc2f699182d6d02697fe6b60db8cda826c4ba
Luuk <twinonedevs@gmail.com>
2018-10-15 21:23:52 +0200

    Add some debugging on the test cases

--- zeos/user.c
+++ zeos/user.c
@@ -24,8 +24,39 @@ int __attribute__ ((__section__(".text.main")))
 
 
 	w("Hello from userland\n");
-	//runjp_rank(0, 9);
-	//while(1);
+	//runjp_rank(0, 8); // OK
+	//runjp_rank(9, 13); // OK 9-13 WORK
+	// runjp_rank(9, 14); // OK 9-14 WORK BUT page fault at the END
+	// runjp_rank(14, 14); // OK 14 only, also works (probably bad memory freeing somewhere (where?))
+
+	// runjp_rank(15, 15); // page fault
+	// runjp_rank(16, 16); // page fault
+
+
+	// runjp_rank(17, 17); // does NOT work [forks, no free process available, page fault]
+		// we don't understand how this has no free processes in the free queue, since our fork bomb works
+	
+	// runjp_rank(18, 18); // OK 
+	// runjp_rank(19, 20); // medium medium (no errno) // no time to change wrapper now, we need the failing tests to pass
+	
+	// runjp_rank(21, 21); // BAD & page fault (???)
+	// runjp_rank(22, 22); // OK
+	// runjp_rank(23, 23); // BAD INITTEST: unable to initialize semaphore 0
+	// runjp_rank(24, 32); // OK x9
+
+	// Results:
+	// 0-14, 18, 22, 24-32 OK
+	// 19, 20 MEDIUM
+	// 15, 16, 17, 21, 23: BAD / ERROR
+
+	// SUM:
+	// 26 OK
+	// 2  MEDIUM
+	// 5  BAD / ERROR
+
+	
+	
+	while(1);
 
 	// pseudo fork bomb works
 	for (int i = 0; i < 1000; i++) {
